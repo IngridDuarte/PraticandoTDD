4da2d06389e0b94d2310494ab57d737ca0bf3343
ingridDuarte <ingrid.b.duarte.id.id@gmail.com>
2019-11-07 02:29:28 -0300

    initial commit

---
+++ src/test/java/tdd/praticando/TddApplicationTests.java
@@ -0,0 +1,13 @@
+package tdd.praticando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
